d60c04d7e21d0e20bde4ed388b1f85c94cbe17fd
ira089 <gvir2015@gmail.com>
2024-08-24 17:19:05 +0300

    start2App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,59 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+// import { useDispatch, useSelector } from 'react-redux';
+// import { ToastContainer } from 'react-toastify';
+// import 'react-toastify/dist/ReactToastify.css';
+// import { lazy } from 'react';
+import PrivateRoute from './PrivateRoute/PrivateRoute';
+import PublicRoute from './PublicRoute/PublicRoute';
+import SharedLayout from './SharedLayout/SharedLayout';
+import HomePage from 'pages/HomePage/HomePage';
+import RegisterPage from 'pages/RegisterPage/RegisterPage';
+import LoginPage from 'pages/Loginpage/LoginPage';
+import MedicineStorePage from 'pages/MedicineStorePage/MedicineStorePage';
+import MedicinePage from 'pages/MedicinePage/MedicinePage';
+import CartPage from 'pages/CartPage/CartPage';
+import ProductPage from 'pages/ProductPage/ProductPage';
+import NotFoundPage from 'pages/NotFoundPage/NotFoundPage';
+// import Loader from './Loader/Loader';
+
+// const HomePage = lazy(() => import('Pages/HomePage/HomePage'));
+
+
+const App = () => {
+  // const dispatch = useDispatch();
+  // const isRefreshing = useSelector(selectIsRefreshing);
+  const isRefreshing = false
+  // useEffect(() => {
+  //   dispatch(refresThunk());
+  // }, [dispatch]);
+
+  return isRefreshing ? (
+    'Loading...'
+  ) : (
+    <>
+      <Routes>
+        <Route path="/" element={<SharedLayout />}>
+          <Route index element={<HomePage />} />
+          <Route path="produst" element={<ProductPage/>} />
+          
+
+          <Route element={<PublicRoute />}>
+            <Route path="register" element={<RegisterPage />} />
+            <Route path="login" element={<LoginPage />} />
+            <Route path="medicine-store" element={<MedicineStorePage />} />
+          </Route>
+          <Route element={<PrivateRoute />}>
+            <Route path="medicine" element={<MedicinePage />} />
+            <Route path="cart" element={<CartPage />} />
+          </Route>
+
+          <Route path="*" element={<NotFoundPage />} />
+        </Route>
+      </Routes>
+      {/* <ToastContainer autoClose={3000} /> */}
+    </>
   );
 };
+
+export default App;
---
+++ src/components/PrivateRoute/PrivateRoute.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export const PrivateRoute = () => {
+  return (
+    <div>PrivateRoute</div>
+  )
+}
+export default PrivateRoute
