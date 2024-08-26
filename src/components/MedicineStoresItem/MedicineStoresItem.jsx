3e25cdfa9b719d6cffc452d98a3fedf1c75323a9
ira089 <gvir2015@gmail.com>
2024-08-26 18:28:26 +0300

    MedicineStoresItem1

--- src/components/MedicineStoresItem/MedicineStoresItem.jsx
+++ src/components/MedicineStoresItem/MedicineStoresItem.jsx
@@ -1,12 +1,29 @@
 import React from 'react';
 import styles from './medicineStoresItem.module.css';
+import { NavLink } from 'react-router-dom';
+import Icon from 'components/Icon/Icon';
 
 const MedicineStoresItem = () => {
   return (
-    <div className={styles.wrapItem}>
+    <NavLink>
+<div className={styles.wrapItem}>
+      
+      <div>
+        <div>
+          <h5>Baumbach LLC</h5>
+          <div>
+            <Icon />
+            <div>
+              <span>open</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    
+  </div>
+    </NavLink>
+    
+  );
+};
 
-    </div>
-  )
-}
-
-export default MedicineStoresItem
+export default MedicineStoresItem;
