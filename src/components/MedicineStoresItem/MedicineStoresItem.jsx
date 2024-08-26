92c7bc0d3a69e57325b82c1e72bad239a95947fe
ira089 <gvir2015@gmail.com>
2024-08-27 00:02:31 +0300

    MedicineStoresItem

--- src/components/MedicineStoresItem/MedicineStoresItem.jsx
+++ src/components/MedicineStoresItem/MedicineStoresItem.jsx
@@ -1,28 +1,35 @@
 import React from 'react';
-import styles from './medicineStoresItem.module.css';
 import { NavLink } from 'react-router-dom';
 import Icon from 'components/Icon/Icon';
+import styles from './medicineStoresItem.module.css';
 
 const MedicineStoresItem = () => {
   return (
-    <NavLink>
-<div className={styles.wrapItem}>
-      
-      <div>
-        <div>
-          <h5>Baumbach LLC</h5>
-          <div>
-            <Icon />
-            <div>
-              <span>open</span>
+    <NavLink className= {styles.wrapItem}>
+      {/* <div className={styles.wrapItem}> */}
+        <div className={styles.titleStar}>
+          <h5 className={styles.title}>Baumbach LLC</h5>
+          <div className={styles.star}>
+            <Icon width={16} height={16} name={'icon-star'} />
+            <spav>3</spav>
+            <div className={styles.open}>
+              open
             </div>
           </div>
         </div>
-      </div>
-    
-  </div>
+        <div className= {styles.addresPhone}>
+        <Icon width={16} fillColor={'none'} strokeColor={'#59B17A'}  height={16} name={'icon-map'} /> 
+        <p>Kretoria 11007 <br />Champerico</p> 
+        </div>
+        <div className= {styles.addresPhone}>
+        <Icon width={16} height={16} fillColor={'none'} strokeColor={'#59B17A'}  name={'icon-phone'} />
+        <p>506-84-9725</p>
+        </div>
+        <div className= {styles.decor1}></div>
+        <div className= {styles.decor2}></div>
+        <div className= {styles.decor3}></div>
+      {/* </div> */}
     </NavLink>
-    
   );
 };
 
