0edffde6f6dac871bf6a66ec083f9f6fbfc9b266
ira089 <gvir2015@gmail.com>
2024-08-27 18:50:15 +0300

    PromoSectionMenu

--- src/components/PromoSection/PromoSectionMenu.jsx
+++ src/components/PromoSection/PromoSectionMenu.jsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import React from 'react';
+import styles from './promoSection.module.css';
+import  Icon  from '../Icon/Icon';
 
 const PromoSectionMenu = () => {
   return (
-    <div>PromoSectionMenu</div>
+    <div className={styles.wrapMenu}>
+        <div className={styles.menu}>
+            <Icon  height={21} width={20} name={"icon-menu"}/>
+            <p>Take user orders form online</p>
+        </div>
+        <div className={styles.menu}>
+            <Icon  height={21} width={20} name={"icon-menu"}/>
+            <p>Create your shop profile</p>
+        </div>
+        
+        <div className={styles.menu}>
+            <Icon  height={21} width={20} name={"icon-menu"}/>
+            <p>Manage your store</p>
+        </div>
+        <div className={styles.menu}>
+            <Icon  height={21} width={20} name={"icon-menu"}/>
+            <p>Get more orders</p>
+        </div>
+        <div className={styles.menu}>
+            <Icon  height={21} width={20} name={"icon-menu"}/>
+            <p>Storage shed</p>
+        </div>
+
+    </div>
   )
 }
 
