161e545ca98c4cc0d236741728d192f56c4d0cab
ira089 <gvir2015@gmail.com>
2024-09-04 20:45:22 +0300

    ReviewsItem

--- src/components/ReviewsItem/ReviewsItem.jsx
+++ src/components/ReviewsItem/ReviewsItem.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import Rev from '../../assets/svg/photo.svg';
+// import Rev from '../../assets/svg/photo.svg';
 import styles from './reviewsItem.module.css';
 
 const ReviewsItem = ({item}) => {
-  const {name,testimonial} = item
+  const {name,testimonial, photo} = item
   return (
     <div className={styles.itemReview}>
-      <img className={styles.img} src={Rev} alt="avatar" />
+      <img className={styles.img} src={photo} alt="avatar" />
       <h3>{name}</h3>
       <p>{testimonial}</p>
     </div>
--- src/components/ReviewsSection/ReviewsSection.jsx
+++ src/components/ReviewsSection/ReviewsSection.jsx
@@ -6,12 +6,21 @@ import Box from '@mui/material/Box';
 import theme from '../../assets/theme';
 import { ThemeProvider } from '@mui/material/styles';
 import ReviewsItem from 'components/ReviewsItem/ReviewsItem';
+import photoMaria from '../../assets/svg/Maria Tkachuk.svg';
+import photoSergey from '../../assets/svg/Sergey Rybachok.svg';
+import photoNata from '../../assets/svg/Natalia Chatuk.svg';
 import styles from './reviewsSection.module.css';
 
 const ReviewsSection = () => {
-  const {reviews } = useSelector(selectReviews)
+  const {reviews } = useSelector(selectReviews);
+  const photo = [photoMaria, photoSergey, photoNata];
+  const reviewsPhoto = reviews.map((item, index) => {
+    return {...item, photo: photo[index]}
+  })
+  console.log(photo)
+  console.log(reviewsPhoto)
 
-  const elements = reviews.map(item => (
+  const elements = reviewsPhoto.map(item => (
     <Grid item mobile={12} tablet={6} desktop={4}>
       <ReviewsItem key={item._id} item={item} />
     </Grid>
