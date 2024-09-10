b45900b79118ad78eb6ee32c21a3616be74d83f2
ira089 <gvir2015@gmail.com>
2024-09-11 00:18:31 +0300

    Update CartPage.jsx

--- src/pages/CartPage/CartPage.jsx
+++ src/pages/CartPage/CartPage.jsx
@@ -8,34 +8,36 @@ import styles from './cartPage.module.css';
 
 const CartPage = () => {
   const { productsUser} = useSelector(selectCart);
-  console.log(productsUser)
+  const cart = productsUser?.map(({idProduct, quantity }) => ({idProduct, quantity}))
+  console.log(cart)
   // const {idProduct, quantity, price} = productsUser;
   const totals = calculateTotals(productsUser);
- console.log(totals)
+ const {total, totalQuantity} = totals
  
-  const [radio, setRadio] = useState('');
+  const [paymentMethod, setPaymentMethod] = useState('');
   const [formData, setFormData] = useState({
     name: '',
     email: '',
     phone: '',
     address: '',
   });
-  console.log(radio)
+  // console.log(radio)
   console.log(formData)
   const handleChangeRadio = (evt) => {
-    setRadio(evt.target.value)
+    setPaymentMethod(evt.target.value)
   }
   
   const handleChangeForm = (values) => {
     console.log('Form values:', values);
     setFormData(values)
-    
   };
-  
+
+  const placeOrder = ({...formData, paymentMethod, total, totalQuantity, ...cart })
+  console.log(placeOrder)
   return (
     <section className={styles.wrapCart}>
     <h2 className={styles.title}>Cart</h2>
-    <CartInfo total={totals.total} handleChange={handleChangeRadio} handleChangeForm={handleChangeForm}/>
+    <CartInfo total={total} handleChange={handleChangeRadio} handleChangeForm={handleChangeForm}/>
     <CartProduts />
 </section>
   )
