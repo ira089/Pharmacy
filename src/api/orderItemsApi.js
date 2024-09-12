b1363d34f9ebba9f9ca35cabe1d04e3e8bca4aa9
ira089 <gvir2015@gmail.com>
2024-09-12 18:05:10 +0300

    body in orderItemAll

--- src/api/orderItemsApi.js
+++ src/api/orderItemsApi.js
@@ -13,22 +13,22 @@ export const fetchOrderItemAdd = async body => {
 };
 
 export const fetchOrdertItemUpd = async (id, body) => {
-    const { data } = await axios.put(`/orderitems/${id}`, body);
-    console.log(data);
-    setAuthHeader(data.token);
-    return data;
-  };
+  const { data } = await axios.put(`/orderitems/${id}`, body);
+  console.log(data);
+  setAuthHeader(data.token);
+  return data;
+};
 
-  export const fetchOrdertItemAll = async () => {
-    const { data } = await axios.get('/orderitems');
-    console.log(data);
-    setAuthHeader(data.token);
-    return data;
-  };
+export const fetchOrdertItemAll = async body => {
+  const { data } = await axios.get('/orderitems', body);
+  console.log(data);
+  setAuthHeader(data.token);
+  return data;
+};
 
-  export const fetchOrdertItemDel = async id => {
-    const { data } = await axios.delete(`/orderitems/${id}`);
-    console.log(data);
-    setAuthHeader(data.token);
-    return data;
-  };
+export const fetchOrdertItemDel = async id => {
+  const { data } = await axios.delete(`/orderitems/${id}`);
+  console.log(data);
+  setAuthHeader(data.token);
+  return data;
+};
