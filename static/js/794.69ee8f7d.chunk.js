78d0a08f2797b0a2117c59e5790902ff432b62dc
ira089 <137916132+ira089@users.noreply.github.com>
2024-09-15 10:09:42 +0000

    Deploying to gh-pages from @ ira089/Pharmacy@9999ccedae030b1a1536eb218c272abed65ecdcf 🚀

---
+++ static/js/794.69ee8f7d.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[794],{1794:function(e,t,n){n.r(t);var r=n(1087),a=n(3329);t.default=function(){return(0,a.jsxs)("div",{children:[(0,a.jsx)("h1",{children:"Cannot found this page"}),(0,a.jsx)(r.rU,{to:"/",children:"To home page"})]})}}}]);
+//# sourceMappingURL=794.69ee8f7d.chunk.js.map
